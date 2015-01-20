ec8799c7191bb5bed6b3e5df4ffe0028d17422ed
Rob Dugas <rob.dugas@gmail.com>
2015-01-19 18:03:12 -0600

    initial page state and start js method work

--- js/app.js
+++ js/app.js
@@ -4,6 +4,40 @@ $(document).ready(function(){
 	//s start over
 	//enter submit answer
 	//c clear
+
+	//on start button press
+	//hide #questionIntro
+
+	$("#startButton").click(function() {
+		console.log("start clicked");
+		startGame();
+	});
+
+	function startGame(){
+		console.log("game started - showing first Question.");
+		$("#questionIntro").hide();
+		showQuestion("#question1");
+		showAnswers("#answerChoiceList1");
+	}
+
+	function showQuestion(questionNumberId){
+		//hide all questions
+		//show question with Id questionNumberId
+		console.log("showQuestion called with questionNumberId: " + questionNumberId);
+		$(questionNumberId).show();
+	}
+
+	function showAnswers(answerChoiceListId){
+		//hide all questions
+		//show question with Id questionNumberId
+		console.log("showAnswer called with answerChoiceListId: " + answerChoiceListId);
+		$(answerChoiceListId).show();
+	}
+
+	// function logMethodCall () {
+
+	// }
+
 });
 
 
